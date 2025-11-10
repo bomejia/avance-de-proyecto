446e511440b1f6b1a9683537456256491383dce9
bomejia <bomejiarocha@gmail.com>
2025-11-10 14:34:56 -0600

    Refactor Nodo class to improve toString method

--- Nodo (1).java
+++ Nodo (1).java
@@ -3,16 +3,17 @@ public class Nodo {
     String hora;
     Nodo next;
     Nodo prev;
-    
-    public Nodo(String tarea,String hora){
-    this.tarea = tarea;
-    this.hora = hora;
-    this.next = null;
-    this.prev = null;
-    } 
-    @Override
-    public String toString(){
-        return String.valueOf(tarea + hora);
+
+    public Nodo(String tarea, String hora) {
+        this.tarea = tarea;
+        this.hora = hora;
+        this.next = null;
+        this.prev = null;
     }
-    
+
+    @Override
+    public String toString() {
+        return tarea + " (" + hora + ")";
+    }
 }
+
