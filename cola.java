565dab91ece5ca0b1f32cbea6f79dfbeffd5aaa2
bomejia <bomejiarocha@gmail.com>
2025-11-10 14:36:43 -0600

    Refactor cola class and update menu options

--- cola.java
+++ cola.java
@@ -1,51 +1,60 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-public class cola {
-    public static void main(){
+
+public class Cola {
+    public static void main() {
         Queue<Nodo> cola = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
         int opcion;
-        do { 
-            System.out.println("lista cola");
-            System.out.println("1-- agregar tarea");
-            System.out.println("2-- eliminar tarea");
-            System.out.println("3-- mostrar sima");
-            System.out.println("4-- mostrar cola");
+
+        do {
+            System.out.println("\n--- MENÚ COLAS ---");
+            System.out.println("1 - Agregar tarea");
+            System.out.println("2 - Eliminar tarea");
+            System.out.println("3 - Mostrar inicio");
+            System.out.println("4 - Mostrar cola");
+            System.out.println("0 - Salir");
+            System.out.print("Opción: ");
             opcion = sc.nextInt();
-            switch(opcion){
-                case 1:{
-                    System.out.println("agregar tarea ");
-                    String tarea = sc.next();
-                    System.out.println("agrega una hora");
-                    String hora = sc.next();
-                    sc.nextLine();
-                    Nodo N = new Nodo(tarea,hora);
-                    cola.add(N);
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Agrega una tarea: ");
+                    String tarea = sc.nextLine();
+                    System.out.print("Agrega una hora: ");
+                    String hora = sc.nextLine();
+                    cola.add(new Nodo(tarea, hora));
                     break;
-                }
-                case 2:{
-                    if(!cola.isEmpty()){
-                        System.out.println("elemento eliminado"+ cola.poll());
-                    }else{
-                        System.out.println("cola vacia");
+
+                case 2:
+                    if (!cola.isEmpty()) {
+                        System.out.println("Elemento eliminado: " + cola.poll());
+                    } else {
+                        System.out.println("Cola vacía");
                     }
                     break;
-                }
-                case 3:{
-                    if(!cola.isEmpty()){
-                        System.out.println("inicio de la cola"+ cola.peek());
-                    }else{
-                        System.out.println("cola vacia");
+
+                case 3:
+                    if (!cola.isEmpty()) {
+                        System.out.println("Inicio de la cola: " + cola.peek());
+                    } else {
+                        System.out.println("Cola vacía");
                     }
                     break;
-                }
-                case 4:{
-                    System.out.println("contenido en cola"+ cola);
-                }
+
+                case 4:
+                    System.out.println("Contenido en la cola: " + cola);
+                    break;
+
+                case 0:
+                    System.out.println("Saliendo del menú de colas...");
+                    break;
+
                 default:
-                    System.out.println("opcion no valida");
+                    System.out.println("Opción no válida");
             }
-        } while (opcion!=0);  
+        } while (opcion != 0);
     }
 }
